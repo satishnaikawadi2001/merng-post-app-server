359c43597f991ac8dee1c218a9648253992872f7
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-07-12 17:27:04 +0530

    deploying

---
+++ index.js
@@ -0,0 +1,28 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const { MONGO_URI } = require('./config.js');
+const Post = require('./models/Post');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const PORT = process.env.port || 5000;
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context   : ({ req }) => ({ req })
+});
+
+mongoose
+	.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log('MongoDB connected');
+		return server.listen({ port: PORT });
+	})
+	.then((res) => {
+		console.log(`server running at ${res.url}`);
+	})
+	.catch((err) => {
+		console.error(err);
+	});
