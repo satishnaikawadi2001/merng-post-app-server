f085e365581f66d5be96d401730097d536ef82fe
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-07-12 17:55:49 +0530

    fixed PORT

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Post = require('./models/Post');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
 	typeDefs,
