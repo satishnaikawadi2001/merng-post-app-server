a13a2134042164a8b8f323726bd8ffacc1ce7ca9
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-07-12 18:06:29 +0530

    fixed config

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	MONGO_URI      :
+		'mongodb+srv://satishnaikawadi:satish2001@cluster0.h5mih.mongodb.net/merng?retryWrites=true&w=majority',
+	JWT_SECRET_KEY : 'this is my secret for jwt'
+};
